9a98cf39483de142f42a2fab46c2ddfcb97b066b
kaledmolina <kaledmoly@gmail.com>
2026-02-13 16:50:09 -0500

    fix: Corrected the import path for `auth_provider.dart` in `streak_modal.dart`.

--- lib/features/home/presentation/widgets/streak_modal.dart
+++ lib/features/home/presentation/widgets/streak_modal.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../auth/presentation/auth_provider.dart';
+import '../../../auth/presentation/auth_provider.dart';
 
 class StreakModal extends StatelessWidget {
   const StreakModal({super.key});
